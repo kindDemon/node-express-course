c5834bdb1e9771f0dc8395b81cf3483a9c160a3a
kindDemon <349448341@qq.com>
2021-08-18 11:58:31 +0800

    add login POST route

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
+app.use(bodyParser.json())
 
 const mockUserData=[
     {name:'Mark'},
     {name:'Jill'}
 ]
+
+app.post('/login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = 'bill';
+    const mockPassword = 'gates';
+
+    if(username == mockUsername && password == mockPassword){
+        res.json({
+            success: true,
+            message: 'match!',
+            token: 'pass token'
+        })
+    }else{
+        res.json({
+            success: false,
+            message: 'username and password do not match'
+        })
+    }
+})
+
 app.get('/users', function(req,res){
     res.json({
         success: true,
